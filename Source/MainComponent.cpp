894e1bf501d645bf926c1e062b90aa657002f5de
Max Walley <58304135+maxwalley@users.noreply.github.com>
2024-11-09 12:32:41 +0000

    Added a small wiimote connection test

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,12 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    wiimotes = wiiuse_init(maxWiiMotes);
+    
+    connectButton.setButtonText("Connect");
+    connectButton.addListener(this);
+    addAndMakeVisible(connectButton);
+    
     setSize(600, 400);
 }
 
@@ -12,9 +18,22 @@ void MainComponent::paint(juce::Graphics& g)
 
     g.setFont(juce::FontOptions(16.0f));
     g.setColour(juce::Colours::white);
-    g.drawText("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText(juce::String(numConnected) + " controllers connected", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
 {
+    connectButton.setBounds(getWidth() / 2 - 75, getHeight() / 2 - + 30, 150, 20);
+}
+
+void MainComponent::buttonClicked(juce::Button* button)
+{
+    if(button == &connectButton && wiimotes)
+    {
+        connectButton.setButtonText("Connecting");
+        
+        numConnected = wiiuse_find(wiimotes, maxWiiMotes, 1);
+        
+        repaint();
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "wiiuse.h"
 
 //==============================================================================
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::Component,
+                       public juce::Button::Listener
 {
 public:
     MainComponent();
@@ -13,5 +15,13 @@ public:
     void resized() override;
 
 private:
+    void buttonClicked(juce::Button* button) override;
+    
+    static constexpr int maxWiiMotes = 4;
+    wiimote** wiimotes = nullptr;
+    int numConnected = 0;
+    
+    juce::TextButton connectButton;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
